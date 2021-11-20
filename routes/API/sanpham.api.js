41dce9dbc243bcdb0f78292cab57ebdebfa6306a
Qui Phan <pvqui0910@gmail.com>
2021-11-20 22:21:41 +0700

    Update sanpham.api.js

--- routes/API/sanpham.api.js
+++ routes/API/sanpham.api.js
@@ -131,24 +131,24 @@ app.route('/detail/:userId')
 
 //5 San pham co gia cao nhat
 app.route('/get/MaxPrice').get(async function (req, res) {
-    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null)  order by giamuangay desc LIMIT :limit', { limit: 5, ngayketthuc: new Date() })
+    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null) order by giamuangay desc LIMIT :limit', { limit: 5, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 //5 San pham moi nhat
 app.route('/get/New').get(async function (req, res) {
-    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, ngayketthuc: new Date() })
+    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 
 //5 San pham moi nhat cung danh muc
 app.route('/get/New/:sanpham/:danhmuc').get(async function (req, res) {
-    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where masanpham != :sanpham and madanhmuc = :danhmuc and (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, sanpham: req.params.sanpham, danhmuc: req.params.danhmuc, ngayketthuc: new Date() })
+    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham where masanpham != :sanpham and madanhmuc = :danhmuc and (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, sanpham: req.params.sanpham, danhmuc: req.params.danhmuc, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 
 //5 San pham moi nhat cung danh muc cha va con
 app.route('/get/New/:danhmuc').get(async function (req, res) {
-    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham s join danhmuc d on s.madanhmuc = d.madanhmuc where d.madanhmuc = :danhmuc or d.madanhmuccha = :danhmuc and (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, danhmuc: req.params.danhmuc, ngayketthuc: new Date() })
+    let sp = await SanPham.selectRawQuery('SELECT * FROM sanpham s join danhmuc d on s.madanhmuc = d.madanhmuc where d.madanhmuc = :danhmuc or d.madanhmuccha = :danhmuc and (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null) order by ngaydang desc LIMIT :limit', { limit: 5, danhmuc: req.params.danhmuc, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 
@@ -163,7 +163,7 @@ app.route('/get/NearEnd').get(async function (req, res) {
 app.route('/get/Name').get(async function (req, res) {
     const queryObject = url.parse(req.url, true).query;
     let sql = `SELECT (select count(masanpham) from sanpham WHERE lower(tensanpham) like :name) as sl,* FROM sanpham 
-    WHERE lower(tensanpham) like :name and (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null)`;
+    WHERE lower(tensanpham) like :name and (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null)`;
     (queryObject.orderType) ? sql += " order by " + queryObject.orderType : sql += "";
     (queryObject.orderBy) ? sql += " desc" : sql += "";
     sql += ` LIMIT 8 OFFSET :offset`
@@ -171,15 +171,15 @@ app.route('/get/Name').get(async function (req, res) {
     res.status(200).json(sp);
 })
 app.route('/get/Count').get(async function (req, res) {
-    let sp = await SanPham.selectRawQuery(`SELECT * FROM sanpham where (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null)
-    order by luot_ra_gia_hien_tai desc
+    let sp = await SanPham.selectRawQuery(`SELECT * FROM sanpham join lichsudaugia on sanpham.malichsucaonhat = lichsudaugia.malichsudaugia where (sanpham.ngayketthuc > :ngayketthuc and sanpham.nguoichienthang is null) 
+    order by lichsudaugia.gia
     LIMIT :limit`, { limit: 5, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 // Lay san pham theo danh muc
 app.route('/get/category/:id').get(async function (req, res) {
     let sp = await SanPham.selectRawQuery(`SELECT * FROM sanpham 
-    WHERE madanhmuc=:id and (sanpham.ngayketthuc < :ngayketthuc or sanpham.nguoichienthang is null)`, { id: req.params.id, ngayketthuc: new Date() })
+    WHERE madanhmuc=:id and (sanpham.ngayketthuc > :ngayketthuc or sanpham.nguoichienthang is null)`, { id: req.params.id, ngayketthuc: new Date() })
     res.status(200).json(sp);
 })
 module.exports = app;
